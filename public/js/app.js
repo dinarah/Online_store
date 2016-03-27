0a48c3af06f256c76cc14c2e6cc83487168738f7
Dinara Khubieva <dinayka@mail.ru>
2016-03-27 20:32:15 +0200

    Switched to frontend logic for getting the products

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,103 @@
+// products
+var jeweleries = [
+  {   sku: "VAL",
+      name: 'Valerie',
+      price: 14.99,
+      description: 'This is our new brand.I am a product description. I’m a great place to include more information about your product. Buyers like to know what they’re getting before they purchase.',
+      canPurchase: true,
+      images: [
+          {
+              full: 'images/download.jpg',
+          }]
+  },
+  {   sku: "LAL",
+      name: 'Lala',
+      price: 15.99,
+      description: 'This is what we can offer to you.I am a product description. I’m a great place to include more information about your product. Buyers like to know what they’re getting before they purchase.',
+      canPurchase: true,
+       images: [
+          {
+              full: 'images/download.jpg',
+          }]
+  },
+     {
+      sku: "NOR",   
+      name: 'Nora',
+      price: 16.99,
+      description: '',
+      canPurchase: true,
+          images: [
+          {
+              full: 'images/download.jpg',
+          }]
+  },
+     {
+      sku: "SUS",   
+      name: 'Susanne',
+      price: 17.99,
+      description: 'This is what we can offer to you',
+      canPurchase: true,
+          images: [
+          {
+              full: 'images/download.jpg',
+          }]
+  },
+{     sku: "JUL",
+      name: 'Julia',
+      price: 18.99,
+      description: 'This is what we can offer to you',
+      canPurchase: true,
+          images: [
+          {
+              full: 'images/download.jpg',
+          }]
+},
+    {
+      sku: "TIN",  
+      name: 'Tina',
+      price: 19.99,
+      description: 'This is what we can offer to you',
+      canPurchase: true,
+          images: [
+          {
+              full: 'images/download.jpg',
+          }]
+    },
+    {
+      sku: "NIN",  
+      name: 'Nina',
+      price: 20.99,
+      description: 'This is what we can offer to you',
+      canPurchase: true,
+          images: [
+          {
+              full: 'images/download.jpg',
+          }]
+    },
+    {
+      sku: "LAR",  
+      name: 'Lara',
+      price: 21.99,
+      description: 'This is what we can offer to you',
+      canPurchase: true,
+          images: [
+          {
+              full: 'images/download.jpg',
+          }]
+    },
+   
+
+];
+
+function getProductBySku(sku)
+{
+    for (var i = 0; i < jeweleries.length; i++) {
+        if (jeweleries[i].sku == sku) {
+            return jeweleries[i];
+        }
+    }
+}
+
 // test comment
 var app = angular.module('OnlineShop', ['ui.router', "store", "myApp", 'ngCart']);
 
@@ -41,18 +141,25 @@ app.config(function ($stateProvider, $urlRouterProvider) {
 });
 
 app.controller('StoreController', ['$http', function ($http) {
-    var store = this;
-    store.items = [];
+    // Without server
+    this.items = _.chunk(jeweleries, 4);
+    
+    // With server
+    /*var store = this;
     $http.get('/items').success(function (data) {
         store.items = _.chunk(data, 4);
-    });
+    });*/
 }]);
 
-app.controller('ProductController', function ($http, $stateParams) {  
-    var productCntrl = this; 
+app.controller('ProductController', function ($http, $stateParams) {
+    // Without server
+    this.item = getProductBySku($stateParams.productSku);
+    
+    // With server
+    /*var productCntrl = this;
     $http.get('/items/' + $stateParams.productSku).success(function (data) {
         productCntrl.item = data;
-    });
+    });*/
 });
 
 app.controller('TabController', function () {
