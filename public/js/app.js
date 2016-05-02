1ed5cfa717df74a3f96a872a4f6f4ed26f8b825a
Dinara Khubieva <dinayka@mail.ru>
2016-05-02 21:38:54 +0200

    changing product name

--- public/js/app.js
+++ public/js/app.js
@@ -1,108 +1,347 @@
 // products
 var jeweleries = [
-  {   sku: "VAL",
-      name: 'Valerie',
-      price: 14.99,
-      description: 'This is our new brand.I am a product description. I’m a great place to include more information about your product. Buyers like to know what they’re getting before they purchase.',
-      canPurchase: true,
-      images: [
-          {
-              full: 'images/download.jpg',
-          }]
+    {
+        sku: "001",
+        name: 'Ring "Valerie"',
+        price: 14.99,
+        description: 'This is our new brand. I am a product description. I’m a great place to include more information about your product. Buyers like to know what they’re getting before they purchase.',
+        canPurchase: true,
+        images: [
+            {
+                full: 'images/full001.jpg',
+                thumbnail: 'images/thumb001.jpg',
+            }
+        ]
   },
-  {   sku: "LAL",
-      name: 'Lala',
-      price: 15.99,
-      description: 'This is what we can offer to you.I am a product description. I’m a great place to include more information about your product. Buyers like to know what they’re getting before they purchase.',
-      canPurchase: true,
-       images: [
-          {
-              full: 'images/download.jpg',
-          }]
+    {
+        sku: "002",
+        name: 'Earrings "Valerie"',
+        price: 15.99,
+        description: 'This is what we can offer to you.I am a product description. I’m a great place to include more information about your product. Buyers like to know what they’re getting before they purchase.',
+        canPurchase: true,
+        images: [
+            {
+                full: 'images/full002.jpg',
+                thumbnail: 'images/thumb002.jpg',
+            }
+        ]
   },
-     {
-      sku: "NOR",   
-      name: 'Nora',
-      price: 16.99,
-      description: '',
-      canPurchase: true,
-          images: [
-          {
-              full: 'images/download.jpg',
-          }]
+    {
+        sku: "003",
+        name: 'Ring "Nora"',
+        price: 16.99,
+        description: '',
+        canPurchase: true,
+        images: [
+            {
+                full: 'images/full003.jpg',
+                thumbnail: 'images/thumb003.jpg',
+            }
+        ]
   },
-     {
-      sku: "SUS",   
-      name: 'Susanne',
-      price: 17.99,
-      description: 'This is what we can offer to you',
-      canPurchase: true,
-          images: [
-          {
-              full: 'images/download.jpg',
-          }]
+    {
+        sku: "004",
+        name: 'Susanne',
+        price: 17.99,
+        description: 'This is what we can offer to you',
+        canPurchase: true,
+        images: [
+            {
+                full: 'images/full004.jpg',
+                thumbnail: 'images/thumb004.jpg',
+            }
+        ]
   },
-{     sku: "JUL",
-      name: 'Julia',
-      price: 18.99,
-      description: 'This is what we can offer to you',
-      canPurchase: true,
-          images: [
-          {
-              full: 'images/download.jpg',
-          }]
+    {
+        sku: "005",
+        name: 'Earrings "Julia"',
+        price: 18.99,
+        description: 'This is what we can offer to you',
+        canPurchase: true,
+        images: [
+            {
+                full: 'images/full005.jpg',
+                thumbnail: 'images/thumb005.jpg',
+            }
+        ]
 },
     {
-      sku: "TIN",  
-      name: 'Tina',
-      price: 19.99,
-      description: 'This is what we can offer to you',
-      canPurchase: true,
-          images: [
-          {
-              full: 'images/download.jpg',
-          }]
+        sku: "006",
+        name: 'Earrings "Tina"',
+        price: 19.99,
+        description: 'This is what we can offer to you',
+        canPurchase: true,
+        images: [
+            {
+                full: 'images/full006.jpg',
+                thumbnail: 'images/thumb006.jpg',
+            }
+        ]
     },
-    {
-      sku: "NIN",  
-      name: 'Nina',
-      price: 20.99,
-      description: 'This is what we can offer to you',
-      canPurchase: true,
-          images: [
-          {
-              full: 'images/download.jpg',
-          }]
-    },
-    {
-      sku: "LAR",  
-      name: 'Lara',
-      price: 21.99,
-      description: 'This is what we can offer to you',
-      canPurchase: true,
-          images: [
-          {
-              full: 'images/download.jpg',
-          }]
-    },
-   
 
+    {
+        sku: "007",
+        name: 'Nina',
+        price: 20.99,
+        description: 'This is what we can offer to you',
+        canPurchase: true,
+        images: [
+            {
+                full: 'images/full007.jpg',
+                thumbnail: 'images/thumb007.jpg',
+            }
+        ]
+    },
+    {
+        sku: "008",
+        name: 'Ring "Susanne"',
+        price: 21.99,
+        description: 'This is what we can offer to you',
+        canPurchase: true,
+        images: [
+            {
+                full: 'images/full008.jpg',
+                thumbnail: 'images/thumb008.jpg',
+            }
+        ]
+
+    },
+
+
+    {
+        sku: "009",
+        name: 'Ring "BetterTwo" black',
+        price: 14.99,
+        description: 'This is what we can offer to you',
+        canPurchase: true,
+        images: [
+            {
+                full: 'images/full009.jpg',
+                thumbnail: 'images/thumb009.jpg',
+            }
+        ]
+    },
+
+    {
+        sku: "010",
+        name: 'Ring "BetterTwo"',
+        price: 15.99,
+        description: 'This is what we can offer to you',
+        canPurchase: true,
+        images: [
+            {
+                full: 'images/full010.jpg',
+                thumbnail: 'images/thumb010.jpg',
+            }
+        ]
+    },
+    {
+        sku: "011",
+        name: 'Ring "ThreeWays"',
+        price: 16.99,
+        description: 'This is what we can offer to you',
+        canPurchase: true,
+        images: [
+            {
+                full: 'images/full011.jpg',
+                thumbnail: 'images/thumb011.jpg',
+            }
+        ]
+    },
+
+    {
+        sku: "012",
+        name: 'Ring "Black Cat"',
+        price: 17.99,
+        description: 'This is what we can offer to you',
+        canPurchase: true,
+        images: [
+            {
+                full: 'images/full012.jpg',
+                thumbnail: 'images/thumb012.jpg',
+            }
+        ]
+    },
+
+    {
+        sku: "013",
+        name: 'Ring "White Dog"',
+        price: 18.99,
+        description: 'This is what we can offer to you',
+        canPurchase: true,
+        images: [
+            {
+                full: 'images/full013.jpg',
+                thumbnail: 'images/thumb013.jpg',
+            }
+        ]
+    },
+
+    {
+        sku: "014",
+        name: '',
+        price: 19.99,
+        description: 'This is what we can offer to you',
+        canPurchase: true,
+        images: [
+            {
+                full: 'images/full014.jpg',
+                thumbnail: 'images/thumb014.jpg',
+            }
+        ]
+    },
+    {
+        sku: "015",
+        name: 'Ring "Black Happines"',
+        price: 20.99,
+        description: 'This is what we can offer to you',
+        canPurchase: true,
+        images: [
+            {
+                full: 'images/full015.jpg',
+                thumbnail: 'images/thumb015.jpg',
+            }
+        ]
+    },
+
+    {
+        sku: "016",
+        name: 'Bracelette "Steel"',
+        price: 21.99,
+        description: 'This is what we can offer to you',
+        canPurchase: true,
+        images: [
+            {
+                full: 'images/full016.jpg',
+                thumbnail: 'images/thumb016.jpg',
+            }
+        ]
+    },
+
+    {
+        sku: "017",
+        name: 'Earrings "Lilly"',
+        price: 22.99,
+        description: 'This is what we can offer to you',
+        canPurchase: true,
+        images: [
+            {
+                full: 'images/full017.jpg',
+                thumbnail: 'images/thumb017.jpg',
+            }
+        ]
+    },
+    {
+        sku: "018",
+        name: 'Ring "Lilly"',
+        price: 14.99,
+        description: 'This is what we can offer to you',
+        canPurchase: true,
+        images: [
+            {
+                full: 'images/full018.jpg',
+                thumbnail: 'images/thumb018.jpg',
+            }
+        ]
+    },
+    {
+        sku: "019",
+        name: 'Ring "Lara"',
+        price: 15.99,
+        description: 'This is what we can offer to you',
+        canPurchase: true,
+        images: [
+            {
+                full: 'images/full019.jpg',
+                thumbnail: 'images/thumb019.jpg',
+            }
+        ]
+    },
+    {
+        sku: "020",
+        name: 'Earrings "Lara"',
+        price: 16.99,
+        description: 'This is what we can offer to you',
+        canPurchase: true,
+        images: [
+            {
+                full: 'images/full020.jpg',
+                thumbnail: 'images/thumb020.jpg',
+            }
+        ]
+    },
+    {
+        sku: "021",
+        name: 'Earrings "Marine"',
+        price: 17.99,
+        description: 'This is what we can offer to you',
+        canPurchase: true,
+        images: [
+            {
+                full: 'images/full021.jpg',
+                thumbnail: 'images/thumb021.jpg',
+            }
+        ]
+    },
+
+    {
+        sku: "022",
+        name: 'Ring "Marine"',
+        price: 18.99,
+        description: 'This is what we can offer to you',
+        canPurchase: true,
+        images: [
+            {
+                full: 'images/full022.jpg',
+                thumbnail: 'images/thumb022.jpg',
+            }
+        ]
+    },
+
+    {
+        sku: "023",
+        name: 'Pendant "Peace"',
+        price: 19.99,
+        description: 'This is what we can offer to you',
+        canPurchase: true,
+        images: [
+            {
+                full: 'images/full023.jpg',
+                thumbnail: 'images/thumb023.jpg',
+            }
+        ]
+    },
+
+    {
+        sku: "024",
+        name: 'Ring "Peace"',
+        price: 20.99,
+        description: 'This is what we can offer to you',
+        canPurchase: true,
+        images: [
+            {
+                full: 'images/full024.jpg',
+                thumbnail: 'images/thumb024.jpg',
+            }
+        ]
+    },
 ];
 
-function getProductBySku(sku)
-{
+
+function getProductBySku(sku) {
     for (var i = 0; i < jeweleries.length; i++) {
         if (jeweleries[i].sku == sku) {
             return jeweleries[i];
         }
     }
+
 }
 
-// test comment
-var app = angular.module('OnlineShop', ['ui.router', "store", "myApp", 'ngCart']);
+
+var app = angular.module('OnlineShop', ['ui.router', "store", "myApp", 'ngCart', "exampleDialog", 'ngDialog']);
 
 
-app.config(function ($stateProvider, $urlRouterProvider, $rootScope) {
+app.config(function ($stateProvider, $urlRouterProvider) {
 
     $urlRouterProvider.otherwise('/home');
 
@@ -133,18 +372,25 @@ app.config(function ($stateProvider, $urlRouterProvider, $rootScope) {
         })
         .state('product', {
             url: "/product",
-            templateUrl: 'partials/product.html', 
+            templateUrl: 'partials/product.html'
         })
         .state('product/: sku', {
             url: "/product/{productSku}",
             templateUrl: 'partials/product.html'
         })
+
+    .state('dialog', {
+        url: "/dialog",
+        templateUrl: 'partials/dialog.html'
+    })
 });
+
 
 app.controller('StoreController', ['$http', function ($http) {
     // Without server
     this.items = _.chunk(jeweleries, 4);
-    
+
+
     // With server
     /*var store = this;
     $http.get('/items').success(function (data) {
@@ -155,7 +401,12 @@ app.controller('StoreController', ['$http', function ($http) {
 app.controller('ProductController', function ($http, $stateParams) {
     // Without server
     this.item = getProductBySku($stateParams.productSku);
-    
+
+
+
+
+
+
     // With server
     /*var productCntrl = this;
     $http.get('/items/' + $stateParams.productSku).success(function (data) {
@@ -173,9 +424,4 @@ app.controller('TabController', function () {
     this.isSelected = function (checkTab) {
         return this.tab === checkTab;
     };
-});
-$rootScope.$on("$routeChangeSuccess", function (event, currentRoute, previousRoute) {
-
-    window.scrollTo(0, 0);
-
 });
--- public/js/shop.js
+++ public/js/shop.js
@@ -1,8 +1,8 @@
 var storeApp = angular.module('store', []);
 
-var myApp = angular.module('myApp',['ngCart']);
+var myApp = angular.module('myApp', ['ngCart']);
 
-myApp.controller ('myCtrl', ['$scope', '$http', 'ngCart', function($scope, $http, ngCart) {
+myApp.controller('myCtrl', ['$scope', '$http', 'ngCart', function ($scope, $http, ngCart) {
     ngCart.setTaxRate(0);
     ngCart.setShipping(0);
 
@@ -26,5 +26,57 @@ myApp.controller ('myCtrl', ['$scope', '$http', 'ngCart', function($scope, $http
 });
 
 productApp.controller('StoreController', function ($scope, $routeParams) {
-    this.productSku = ($routeParams.productSku) ? parseInt($routeParams.productSku) : 0;
+    this.productSku = ($routeParams.productSku) ? parseInt($routeParams.g) : 0;
 });*/
+
+var DialogApp = angular.module('exampleDialog', ['ngDialog']);
+// Example of how to set default values for all dialogs
+DialogApp.config(['ngDialogProvider', function (ngDialogProvider) {
+    ngDialogProvider.setDefaults({
+        className: 'ngdialog-theme-default',
+        plain: false,
+        showClose: true,
+        closeByDocument: true,
+        closeByEscape: true,
+        appendTo: false,
+        preCloseCallback: function () {
+            console.log('default pre-close callback');
+        }
+    });
+        }]);
+app.controller('MainCtrl', function ($scope, $rootScope, ngDialog, $timeout) {
+    $rootScope.jsonData = '{"foo": "bar"}';
+    $rootScope.theme = 'ngdialog-theme-default';
+    $scope.directivePreCloseCallback = function (value) {
+        if (confirm('Close it? MainCtrl.Directive. (Value = ' + value + ')')) {
+            return true;
+        }
+        return false;
+    };
+    $scope.openNotify = function () {
+        var dialog = ngDialog.open({
+            template: '<p>This item added to your cart!</p>' +
+                '<div class="ngdialog-buttons"><button type="button" class="ngdialog-button ngdialog-button-primary" ng-click="closeThisDialog(1)">OK</button></div>',
+            plain: true
+        });
+        dialog.closePromise.then(function (data) {
+            console.log('ngDialog closed' + (data.value === 1 ? ' using the button' : '') + ' and notified by promise: ' + data.id);
+        });
+    };
+
+    $rootScope.$on('ngDialog.opened', function (e, $dialog) {
+        console.log('ngDialog opened: ' + $dialog.attr('id'));
+    });
+    $rootScope.$on('ngDialog.closed', function (e, $dialog) {
+        console.log('ngDialog closed: ' + $dialog.attr('id'));
+    });
+    $rootScope.$on('ngDialog.closing', function (e, $dialog) {
+        console.log('ngDialog closing: ' + $dialog.attr('id'));
+    });
+    $rootScope.$on('ngDialog.templateLoading', function (e, template) {
+        console.log('ngDialog template is loading: ' + template);
+    });
+    $rootScope.$on('ngDialog.templateLoaded', function (e, template) {
+        console.log('ngDialog template loaded: ' + template);
+    });
+})
